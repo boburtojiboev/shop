884e36e539284594cce5971e0dde9d5a668791e6
boburtojiboev <tbobur1995@gmail.com>
2024-01-18 16:36:19 +0900

    feat: Loyihani expressini shakillantirib olish

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://tbobur1995:t27011995rBobur@cluster0.5lxzxir.mongodb.net/Reja";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
