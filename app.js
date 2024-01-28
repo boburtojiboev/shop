918120903fd237cc42703a2aaf4a320ca7bc9267
boburtojiboev <tbobur1995@gmail.com>
2024-01-28 18:14:53 +0900

    feat: create jwt tokens and enable cookie-parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 const router_bssr = require("./router_bssr");
+const cookieParser = require("cookie-parser");
 
 let session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session); 
@@ -19,7 +20,9 @@ const store =  new MongoDBStore({
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+// 2: session code 
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
--- controller/memberController.js
+++ controller/memberController.js
@@ -1,4 +1,7 @@
 const Member = require("../models/Member");
+const jwt = require("jsonwebtoken");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
 
 let memberController = module.exports;
 
@@ -10,7 +13,12 @@ memberController.signup = async (req, res) => {
 
     const member = new Member();
     const new_member = await member.signupData(data);
-     // TODO:  AUTHENTICATE BESED ON JWT
+    // token hosil qilinyabdi
+    const token = memberController.createToken(new_member);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
     console.log(`ERROR, cont/signup, ${err.message}`);
@@ -26,6 +34,13 @@ memberController.login = async (req, res) => {
     const member = new Member();
     const result = await member.loginData(data);
 
+    // token hosil qilinyabdi
+    const token = memberController.createToken(result);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
+
     // console.log("result:::", result);
     res.json({ state: "succeed", data: result });
   } catch (err) {
@@ -37,4 +52,23 @@ memberController.login = async (req, res) => {
 memberController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("Logout sahifadasiz");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_date = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+
+    const token = jwt.sign(upload_date, process.env.SECRET_TOKEN, {
+      expiresIn: "6h",
+    });
+
+    assert.ok(token, Definer.auth_err2);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
