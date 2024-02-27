fa28cffbf476fd32647d6e7773a1a0415275021a
boburtojiboev <tbobur1995@gmail.com>
2024-02-27 22:10:46 +0900

    feat: develop getMemberArticles rest api business logic

--- models/Community.js
+++ models/Community.js
@@ -1,6 +1,7 @@
 const BoArticleModel = require("../schema/bo_article.model");
 const Definer = require("../lib/mistake");
 const { shapeIntoMongooseObjectId } = require("../lib/config");
+const assert = require("assert");
 
 class Community {
   constructor(mb_id) {
@@ -26,6 +27,37 @@ class Community {
     } catch (mongo_err) {
       console.log(mongo_err);
       throw new Error(Definer.auth_err1);
+    }
+  }
+
+  async getMemberArticlesData(member, mb_id, inquery) {
+    try {
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
+      mb_id = shapeIntoMongooseObjectId(mb_id);
+      const page = inquery["page"] ? inquery["page"] * 1 : 1;
+      const limit = inquery["limit"] ? inquery["limit"] * 1 : 5;
+
+      const result = await this.boArticleModel
+        .aggregate([
+          { $match: { mb_id: mb_id, art_status: "active" } },
+          { $sort: { createdAt: -1 } },
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+          {
+            $lookup: {
+              from: "members",
+              localField: "mb_id",
+              foreignField: "_id",
+              as: "member_data",
+            },
+          },
+        ])
+        .exec();
+      assert.ok(result, Definer.article_err2);
+
+      return result;
+    } catch (err) {
+      throw err;
     }
   }
 }
